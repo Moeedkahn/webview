3982043b125fc3b5c3426df631e6bd97796b8313
moeedtriple2 <114418982+MoeedKahn5@users.noreply.github.com>
2023-09-12 13:44:59 +0500

    webview

--- Webview/ViewController.swift
+++ Webview/ViewController.swift
@@ -5,15 +5,20 @@
 //  Created by Moeed Khan on 12/09/2023.
 //
 
+import Foundation
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+class ViewController: UIViewController, WKUIDelegate {
+    
+        @IBOutlet weak var webview: WKWebView!
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            let homeUrl:URL = URL(string: "https://chat.openai.com/")!
+            
+            self.webview.uiDelegate = self
+            webview.allowsBackForwardNavigationGestures = true
+            webview.load(URLRequest(url: homeUrl))
+            
+        }
 }
-
